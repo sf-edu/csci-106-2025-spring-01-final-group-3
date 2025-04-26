81f5294b1a25927971cacbf02935fadedd48b357
ghostbox5830 <216LevelUp@gmail.com>
2025-04-26 14:54:39 -0400

    Edits to Yahoo Finance

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
+using YahooFinanceApi;
+
 class Program
 {
+    static async Task MainAsync()
+    {
+        Console.Write("Please enter a stock you would like to track: ");
+        var ticker = Console.ReadLine().ToUpper();
+        // You could query multiple symbols with multiple fields through the following steps:
+        var securities = await Yahoo
+            .Symbols(ticker)
+            .Fields(Field.Symbol, Field.RegularMarketPrice, Field.FiftyTwoWeekHigh)
+            .QueryAsync();
+        // var aapl = securities["AAPL"][Field.RegularMarketPrice]; // or, you could use aapl.RegularMarketPrice directly for typed-value
+        Console.WriteLine(securities[ticker][Field.FiftyTwoWeekHigh]);
+    }
+
     static void Main()
     {
-        
+        Task.WaitAll(MainAsync());
     }
 }
